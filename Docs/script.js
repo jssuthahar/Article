453ab049a33e4cc48fbb823ba18dfdb164d8015c
Suthahar <jssuthahar@gmail.com>
2025-11-23 19:58:59 +0800

    Implement article filtering functionality
    Added a filter function to search articles by title or description.

--- Docs/script.js
+++ Docs/script.js
@@ -1,3 +1,15 @@
-console.log("Technical Articles Website Loaded");
+function filterArticles() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const articles = document.getElementById('articleList').getElementsByClassName('article-item');
 
-// You can later add search or filter functionality here
+    for (let i = 0; i < articles.length; i++) {
+        let title = articles[i].getElementsByTagName('h3')[0].innerText;
+        let desc = articles[i].getElementsByTagName('p')[0].innerText;
+        if (title.toLowerCase().indexOf(filter) > -1 || desc.toLowerCase().indexOf(filter) > -1) {
+            articles[i].style.display = "";
+        } else {
+            articles[i].style.display = "none";
+        }
+    }
+}
