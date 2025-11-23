cb0ac6fdd9708f87ff72cc0672c1225177cffa87
Suthahar <jssuthahar@gmail.com>
2025-11-23 20:13:54 +0800

    Refactor article filtering logic for clarity

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,16 @@
 function filterArticles() {
-    const input = document.getElementById('searchInput');
-    const filter = input.value.toLowerCase();
-    const articles = document.getElementById('articleList').getElementsByClassName('article-item');
+  const input = document.getElementById('searchInput');
+  const filter = input.value.toLowerCase();
+  const list = document.getElementById('articleList');
+  const articles = list.getElementsByClassName('article-item');
 
-    for (let i = 0; i < articles.length; i++) {
-        let title = articles[i].getElementsByTagName('h3')[0].innerText;
-        let desc = articles[i].getElementsByTagName('p')[0].innerText;
-        if (title.toLowerCase().indexOf(filter) > -1 || desc.toLowerCase().indexOf(filter) > -1) {
-            articles[i].style.display = "";
-        } else {
-            articles[i].style.display = "none";
-        }
+  for (let i = 0; i < articles.length; i++) {
+    const title = articles[i].getElementsByTagName('h3')[0].textContent;
+    const desc = articles[i].getElementsByTagName('p')[0].textContent;
+    if (title.toLowerCase().includes(filter) || desc.toLowerCase().includes(filter)) {
+      articles[i].style.display = "";
+    } else {
+      articles[i].style.display = "none";
     }
+  }
 }
